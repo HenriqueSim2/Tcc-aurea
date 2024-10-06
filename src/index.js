2e2450d0a816ca6c737aa65b7a11a9a2fbb2779d
Seu nome fofo <emailfofo@fofura.com>
2024-10-05 22:44:28 -0300

    s

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
-import App from './app';
+import App from './pages/site empresa/landing.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
